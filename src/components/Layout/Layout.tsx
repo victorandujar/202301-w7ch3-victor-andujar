8c500eb83a18f9adb7cae98d7af8d742ed849757
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-02-21 22:59:14 +0100

    Add useUsers hook to Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,8 +1,21 @@
+import useUsers from "../../hooks/useUsers/useUsers";
 import Form from "../Form/Form";
 import RobotsList from "../RobotsList/RobotsList";
 import LayoutStyled from "./LayoutStyled";
+import { useEffect } from "react";
+
+const userCredentials = {
+  username: "marc4",
+  password: "marc1234",
+};
 
 const Layout = (): JSX.Element => {
+  const { loginUser } = useUsers();
+
+  useEffect(() => {
+    loginUser(userCredentials);
+  }, [loginUser]);
+
   return (
     <LayoutStyled>
       <header className="main-header">
